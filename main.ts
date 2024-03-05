af7227f8bdf363f1debe3b6e3c628954517f679a
awannoman <160035058+awannoman@users.noreply.github.com>
2024-03-05 19:14:10 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+// // Q7,Q8
+console.log(4 + 4);
+console.log(14 - 6);
+console.log(4 * 2);
+console.log(16 / 2);
+
+// Q9
+let favoriteNumber = 5;
+console.log("My favorite Number is",favoriteNumber);
